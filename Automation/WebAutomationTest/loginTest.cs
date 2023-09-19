f066aa0b2df3ca1d7f4e6da28b5570cc87c9b122
ArunThamilarasu <AThamilarasu@ariessys.com>
2023-09-19 09:30:41 +0530

    Added skelton

---
+++ Automation/WebAutomationTest/loginTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace WebAutomationTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
